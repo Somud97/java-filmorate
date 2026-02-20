f22db0b6bab7de5c7f0ec8734780c210717e0abe
s.maksimyuk <s.maksimyuk@yandex.ru>
2026-02-20 21:26:27 +0300

    Sprint 13: класс Director больше не наследуется от DirectorDto

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -3,11 +3,10 @@ package ru.yandex.practicum.filmorate.model;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
-import ru.yandex.practicum.filmorate.model.dto.DirectorDto;
 
 @Setter
 @Getter
-public class Director extends DirectorDto {
+public class Director {
     private int id;
 
     @NotBlank(message = "Имя режиссёра не должно быть пустым")
