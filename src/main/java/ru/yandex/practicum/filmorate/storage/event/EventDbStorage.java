e0b9c926d461ca341e5ba70cdf098668cde77dbf
s.maksimyuk <s.maksimyuk@yandex.ru>
2026-02-20 16:21:59 +0300

    Sprint 13: убираем лишний импорт

--- src/main/java/ru/yandex/practicum/filmorate/storage/event/EventDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/event/EventDbStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.event;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
